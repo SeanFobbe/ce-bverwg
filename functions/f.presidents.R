bf4b0f454207ddd530018b421c0409fd98017976
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2022-07-30 00:54:51 +0200

    finalize presidents fun

---
+++ functions/f.presidents.R
@@ -0,0 +1,51 @@
+
+#' Diese Funktion dokumentiert für jede Entscheidung welche/r Präsident:in oder Vize-Präsident:in am Tag der Entscheidung im Amt war. Datensatz muss nach Datum sortiert sein!
+
+
+#' @param datum Das Datum der Entscheidungen.
+#' @param gericht Das Gericht dessen Führungsriege dokumentiert werden soll.
+#' @param pvp.fcg Eine Ausgabe des Datensatzes: \enquote{Seán Fobbe and Tilko Swalve (2021). Presidents and Vice-Presidents of the Federal Courts of Germany (PVP-FCG). Version 2021-04-08. Zenodo. DOI: 10.5281/zenodo.4568682}
+
+
+
+
+f.presidents <- function(datum,
+                         gericht,
+                         pvp.fcg){
+
+    
+    ## Auf relevantes Gericht reduzieren
+    
+    setDT(pvp.fcg)
+    
+    dt <- pvp.fcg[court == gericht, .(court,
+                                      name_last,
+                                      name_first,
+                                      term_begin_date,
+                                      term_end_date)]
+    
+    ## Hypothethisches Amtsende
+    ## Weil der/die aktuelle (Vize-)Präsident:in noch im Amt ist, ist der Wert für das Amtsende "NA". Dieser ist aber für die verwendete Logik nicht greifbar, weshalb an dieser Stelle ein hypothetisches Amtsende in einem Jahr ab dem Tag der Datensatzerstellung fingiert wird. Es wird nur an dieser Stelle verwendet und danach verworfen.
+
+    dt[is.na(term_end_date)]$term_end_date <- Sys.Date() + 365
+
+    ## Namen initialisieren
+    names <- as.character(datum)
+
+    ## Daten mit Namen ersetzen
+    for (i in seq_len(N)){
+
+        logical <- dt$term_begin_date[i] < as.IDate(datum) &  as.IDate(datum) < dt$term_end_date[i]
+        indices <- which(logical, datum)
+        names[indices] <- dt$name_last[i]
+        
+    }
+
+
+    return(names)
+
+}
+
+
+
+
