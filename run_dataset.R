01f337a7876c68bdb3a11c7b66add9dc0bbb72bd
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2022-07-29 23:30:57 +0200

    rename pipeline rmd and automate project run script

--- run_dataset.R
+++
@@ -1,9 +0,0 @@
-# Run full data set pipeline
-
-dir.create("output", showWarnings = FALSE)
-
-rmarkdown::render("CE-BVerwG_Compilation.Rmd",
-                  output_file = paste0("output/CE-BVerwG_",
-                                       Sys.Date(),
-                                       "_CompilationReport.pdf"))
-
