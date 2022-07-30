fe1af1a5ce4ceb9fba28913ed95b43c8d6cc75e1
Sean Fobbe <70438644+SeanFobbe@users.noreply.github.com>
2022-07-30 18:48:22 +0200

    add url merge to final fun

--- functions/f.dataset_finalize.R
+++ functions/f.dataset_finalize.R
@@ -3,6 +3,7 @@
 #' Der Datensatz wird mit dieser Funktion um bereits berechnete Variablen angereichert und in Reihenfolge der Variablen-Dokumentation des Codebooks sortiert. Sollten die Anzahl oder die Namen der Variablen von denen im Codebook abweichen wird die Funktion mit einer Fehlermeldung abbrechen.
 
 #' @param x Data.table. Der nach Datum sortierte und im Text bereinigte Datensatz.
+#' @param downlod.table Data.table. Die Tabelle mit den Informationen zum Download. Wird mit dem Hauptdatensatz vereinigt.
 #' @param vars.additional Data.table. Zusätzliche Variablen, die zuvor extrahiert wurden und nun mit cbind eingehängt werden. Vektoren müssen so geordnet sein wie 'x'.
 #' @param varnames Character. Die im Datensatz erlaubten Variablen, in der im Codebook vorgegebenen Reihenfolge.
 
@@ -10,13 +11,25 @@
 
 
 f.dataset_finalize <- function(x,
+                               download.table,
                                vars.additional,
                                varnames){
 
 
     ## Bind additional vars
-    dt.final <- cbind(x,
+    dt.main <- cbind(x,
                      vars.additional)
+
+
+    ## Merge URL vector
+    dt.download$doc_id <- gsub("\\.pdf",
+                               "\\.txt",
+                               download.table$doc_id)
+
+    dt.final <- merge(dt.main,
+                      dt.download,
+                      by = "doc_id")
+    
 
     ## Remove LaTeX escape characters
     varnames <- gsub("\\\\", "", varnames)
